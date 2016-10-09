ca65db30aadafda20ee106a54d79c8fa5d12893a
Arunas Slenderis <nasas60@gmail.com>
2016-10-09 21:41:03 +0300

    removing webpack dev tools

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const path = require("path");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-
-const config = require("./webpack.config");
+// const webpack = require("webpack");
+// const webpackDevMiddleware = require("webpack-dev-middleware");
+// const webpackHotMiddleware = require("webpack-hot-middleware");
+//
+// const config = require("./webpack.config");
 
 const app = express();
-const compiler = webpack(config);
+// const compiler = webpack(config);
 const PORT = process.env.PORT || 3000;
 
-app.use(webpackDevMiddleware(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true
-  },
-}));
-app.use(webpackHotMiddleware(compiler));
+// app.use(webpackDevMiddleware(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath,
+//   stats: {
+//     colors: true
+//   },
+// }));
+// app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static("./dist"));
 
