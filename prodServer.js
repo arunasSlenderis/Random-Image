ce6e117327505b15d1b4945fe2fc4d10b044a7b7
Arunas Slenderis <nasas60@gmail.com>
2016-10-30 17:29:57 +0200

    Updated production server to es5 for heroku

--- prodServer.js
+++ prodServer.js
@@ -1,20 +1,99 @@
-import express from "express";
-import bodyParser from "body-parser";
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-import db from "./database/db";
-import * as routes from "./routes";
+var db = require("./database/db");
 
 app.use(express.static("./dist"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", routes.home);
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("src/index.html"));
+});
 
-app.post("/info", routes.info);
+app.post("/info", (req, res) => {
+  let imageId = req.body.imageId;
+  let ip = req.body.ip;
+  let likePressed = req.body.likePressed;
+  let dislikePressed = req.body.dislikePressed;
+
+  db.like.findOne({
+    where: { imageId }
+  })
+  .then(image => {
+    const data = {
+      likes: image ? image.dataValues.likes : "0",
+      dislikes: image ? image.dataValues.dislikes : "0",
+      views : image ? image.dataValues.views : "1"
+    };
+
+    if(!image) {
+      db.like.create({
+        imageId,
+        likes: 0,
+        dislikes: 0,
+        views: 1
+      });
+      db.usersIP.create({
+        imageId,
+        ip,
+        liked_disliked: false
+      });
+      res.send(data);
+    } else {
+      db.usersIP.findAll({
+        where: { imageId }
+      })
+      .then(imagesWithIP => {
+        const imageWithIP = imagesWithIP.find(imageIP => {
+          return ip === imageIP.dataValues.ip;
+        });
+        if(imageWithIP.dataValues.liked_disliked === false) {
+          if(likePressed || dislikePressed) {
+            imageWithIP.update({
+              liked_disliked: true
+            });
+            image.update({
+              likes:
+                likePressed
+                  ?
+                  image.dataValues.likes + 1
+                  :
+                  image.dataValues.likes,
+              dislikes:
+                dislikePressed
+                  ?
+                  image.dataValues.dislikes + 1
+                  :
+                  image.dataValues.dislikes,
+            })
+            .then(updatedField => {
+              data.likes = updatedField.dataValues.likes;
+              data.dislikes = updatedField.dataValues.dislikes;
+              res.send(data);
+            });
+          }
+        } else if(!imageWithIP) {
+          image.update({
+            views: image.dataValues.views + 1
+          })
+          .then(updatedField => {
+            data.views = updatedField.dataValues.views;
+            res.send(data);
+          });
+        }
+      });
+    }
+  })
+  .catch(error => {
+    console.log("Error finding image: ", error);
+  });
+});
 
 db.connection.sync({
   // force: true
